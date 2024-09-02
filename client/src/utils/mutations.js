9cb4eaa38c5db2f8e43b6b1f8e9c30bd6d670160
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-09-02 00:52:50 -0700

    query and mutaion files and code added to front end. App .jsx code is modified

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,59 @@
+import {gpl} from '@apollo/client';
+
+export const LOGIN_USER =gpl `
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+export const ADD_USER =gpl `
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+export const SAVE_BOOK =gpl `
+    mutation saveBook($bookData: BookInput!) {
+        saveBook(bookData: $bookData) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+    `;
+
+export const REMOVE_BOOK =gpl `
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
