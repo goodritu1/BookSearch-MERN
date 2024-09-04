072f248d2755cc6156a80c0a9d8b4773e5c53ed1
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-09-03 17:21:18 -0700

    fixed error in server.js. expressMiddleware was not declared

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('@apollo/server');
 const { typeDefs, resolvers } = require('./schemas');
 const { expressMiddleware}= require('@apollo/server/express4')
 const path = require('path');
 const db = require('./config/connection');
 // const routes = require('./routes');
+const { authMiddleware } = require("./utils/auth");
 // 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startApolloServer = async () => {
+  await server.start();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use ('/graphql',expressMiddleware(server));
+app.use ('/graphql',expressMiddleware(server,{context:authMiddleware}));
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -26,3 +35,6 @@ db.once('open', () => {
 }
   )
 });
+};
+
+startApolloServer();
